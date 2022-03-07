9f9ba87492ff004651260153929a98eab1cb735d
Read Langworthy <rlangworthy@gmail.com>
2022-03-07 11:15:03 -0800

    commit tree ad5b15cae3afe426dceebc8c43938bfc8a2668f7

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,24 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    array = self.artists
+    return_array = []
+    array.each do |artist|
+      return_array.push(artist.name)
+    end 
+    return_array
   end
+
+
+
 end
